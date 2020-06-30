2b8837d75e81407d6efc64a06af017fe9383ada5
Mustafa Abdelrahman <mustafausama@outlook.com>
2020-06-30 20:00:55 +0300

    Semi-ready built

---
+++ build/precache-manifest.6aead05172984227270807ef72640206.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66ab9a170800e4240a54f355beba5507",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ced9a157837c509cccc6",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "55ec05972e1581e8b0ee",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "ced9a157837c509cccc6",
+    "url": "/static/js/2.a25ac87c.chunk.js"
+  },
+  {
+    "revision": "f576af3254df0679363d3e6ffe804e36",
+    "url": "/static/js/2.a25ac87c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55ec05972e1581e8b0ee",
+    "url": "/static/js/main.3dc9230e.chunk.js"
+  },
+  {
+    "revision": "c2e377047a47cd6fa16e",
+    "url": "/static/js/runtime-main.1d297b98.js"
+  }
+]);
